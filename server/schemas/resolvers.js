8c3e1cb47b8c9b6092a06fbd6e74ab710cec76d0
Jonathan Martinez <jon@Jonathans-MacBook-Pro.local>
2022-04-23 13:53:10 -0500

    starter code for everyone to work off

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,15 @@
+// Insert models into curly braces below
+const { } = require('../models');
+
+const resolvers = {
+  // Queries and mutations go here
+};
+
+
+
+
+
+
+
+
+module.exports = resolvers;
