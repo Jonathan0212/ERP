594350556a4c330563e64d137f2ee5ea9b19495c
Jeremy O'Daniel <jeremy.odaniel@gmail.com>
2022-04-27 23:44:54 -0500

    Add JWTs for authentication

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,14 +14,17 @@ const typeDefs = gql`
   }
 
   type Location {
+    _id: ID
     location: String
     inventory: [Inventory]
   }
   type Inventory {
+    _id: ID
     inventoryQty: Int
     category: [Category]
   }
   type Category {
+    _id: ID
     categoryType: String
     categoryQty: Int
     unitQty: Int
@@ -29,15 +32,18 @@ const typeDefs = gql`
   }
   
   type Query {
+    me: User
     users: [User]
-    user(username: String!): User
-    location(username: String): [Location]
+    user(username: String!): User   # Change to Auth to allow JWTs
+    locations(username: String): [Location]
+    location(_id: ID!): Location
     inventory: [Inventory]
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+  }
 `;
 
 
