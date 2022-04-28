2fa2ef0f29eeff5cb82830f1e459ba12fa4abfcf
Blake Elliott <blakeelliott@Blakes-MacBook-Pro.local>
2022-04-27 21:20:40 -0500

    updated

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,11 +3,11 @@ function Landing() {
 
     return (
         <div className="container-fluid">
-            <div className="row">
-                <div className="col-8">
+            <div className="row mt-3">
+                <div className="col-12 col-md-8">
                     <img src={logo} style={{ maxWidth: "90%" }} />
                 </div>
-                <div className="col-4">
+                <div className="col-12 col-md-4">
                     <p>An application to manage craft beer inventory..
                         Users will be able to track the quantity, production cost, profit, sales, etc.</p>
 
--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ import {
 } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3005/graphql',
+  uri: 'http://localhost:3000/graphql',
   cache: new InMemoryCache()
 });
 const root = ReactDOM.createRoot(document.getElementById('root'));
